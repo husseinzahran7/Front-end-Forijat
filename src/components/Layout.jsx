d23dbbea512874eec8bfbe215d7be14bd555d687
Hussein zahran <139020942+husseinzahran7@users.noreply.github.com>
2024-11-30 18:02:40 +0300

    fixed margin for layout

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -10,11 +10,12 @@ function Layout({ children }) {
     
     <Box
 
-    sx={{mt:4,mb:0, display: 'flex', flexDirection: 'column', minHeight: '100vh',minWidth: '80vw', 
+    sx={{mt:{xs:10,md:10},mb:0, display: 'flex', flexDirection: 'column', minHeight: '100vh',minWidth: '80vw', 
     // border: '1px solid red'
      }}>
+      <Box>
     <Navbar />
-
+    </Box>
       {/* <Container component="main" sx={{ mt: 0, mb: 0, p: 0,m:0, flex: 1}}maxWidth={false}> */}
         {children}
       {/* </Container> */}
